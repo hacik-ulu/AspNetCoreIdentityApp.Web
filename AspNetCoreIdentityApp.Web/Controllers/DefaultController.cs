890d861e3a80f288667ed9195e4c736f8c80b634
Haçik Ulu <hacikd2@gmail.com>
2025-04-24 21:22:43 +0300

    editing.

---
+++ AspNetCoreIdentityApp.Web/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using AspNetCoreIdentityApp.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreIdentityApp.Web.Controllers
+{
+    public class DefaultController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpViewModel request)
+        {
+            return View();
+        }
+    }
+}
