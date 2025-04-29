1bbbe7185a1345aa284b3b38ec7889313debe889
Haçik Ulu <hacikd2@gmail.com>
2025-04-29 22:38:12 +0300

    Member page editing.

--- AspNetCoreIdentityApp.Web/Controllers/MemberController.cs
+++ AspNetCoreIdentityApp.Web/Controllers/MemberController.cs
@@ -1,4 +1,5 @@
 ﻿using AspNetCoreIdentityApp.Web.Models.Identity;
+using AspNetCoreIdentityApp.Web.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -9,15 +10,26 @@ namespace AspNetCoreIdentityApp.Web.Controllers
     public class MemberController : Controller
     {
         private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
 
-        public MemberController(SignInManager<AppUser> signInManager)
+        public MemberController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
         {
             _signInManager = signInManager;
+            _userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var currentUser = await _userManager.FindByNameAsync(User.Identity!.Name!);
+
+            var userViewModel = new UserInfoViewModel()
+            {
+                Email = currentUser!.Email!,
+                UserName = currentUser.UserName,
+                PhoneNumber = currentUser.PhoneNumber
+            };
+
+            return View(userViewModel);
         }
 
         public async Task<IActionResult> Logout()
---
+++ AspNetCoreIdentityApp.Web/ViewModels/UserInfoViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AspNetCoreIdentityApp.Web.ViewModels
+{
+    public class UserInfoViewModel
+    {
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
