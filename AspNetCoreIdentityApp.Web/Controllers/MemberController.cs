4672bd1191d13ba4098410e12dbb66500d4a3496
Haçik Ulu <hacikd2@gmail.com>
2025-04-29 23:12:57 +0300

    ChangePassword edited.

--- AspNetCoreIdentityApp.Web/Controllers/MemberController.cs
+++ AspNetCoreIdentityApp.Web/Controllers/MemberController.cs
@@ -1,4 +1,5 @@
-﻿using AspNetCoreIdentityApp.Web.Models.Identity;
+﻿using AspNetCoreIdentityApp.Web.Extensions;
+using AspNetCoreIdentityApp.Web.Models.Identity;
 using AspNetCoreIdentityApp.Web.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -36,6 +37,51 @@ namespace AspNetCoreIdentityApp.Web.Controllers
         {
             await _signInManager.SignOutAsync();
             return RedirectToAction("Index", "Default");
+        }
+
+        public IActionResult PasswordChange()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PasswordChange(PasswordChangeViewModel request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var currentUser = await _userManager.FindByNameAsync(User.Identity!.Name!);
+
+            // Eski password kontrolü
+            var checkOldPassword = await _userManager.CheckPasswordAsync(currentUser, request.PasswordOld);
+
+            if (!checkOldPassword)
+            {
+                ModelState.AddModelError(string.Empty, "Eski şifreniz yanlış");
+                return View();
+            }
+
+            var resultChangePassword = await _userManager.ChangePasswordAsync(currentUser, request.PasswordOld, request.PasswordNew);
+
+            if (!resultChangePassword.Succeeded)
+            {
+
+                ModelState.AddModelErrorList(resultChangePassword.Errors.Select(x => x.Description).ToList());
+            }
+
+
+            // Şifre değiştiğinden security stamp değerini güncelleyerek başka bir sekmede de açık kalma ihtimalinden ötürü bilgileri güncelliyoruz.
+            await _userManager.UpdateSecurityStampAsync(currentUser);
+
+            // Şifre değiştiğinden signout yapıp sonra tekrardan signIn yapacağız.
+            await _signInManager.SignOutAsync();
+            await _signInManager.PasswordSignInAsync(currentUser, request.PasswordNew, true, false);
+
+            TempData["SuccessMessage"] = "Şifreniz başarıyla gerçekleşmiştir.";
+
+            return View();
         }
     }
 }
